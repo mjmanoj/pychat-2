32ac922b69ea5fb1d2f1aa85171f696162a56256
marcusw@cox.net <marcusw@cox.net@e19e3dd8-ee72-11de-a004-2f18c49ebcc3>
2010-01-06 00:52:58 +0000

    Made branch for encrypted communications.

---
+++ branches/crypt/server.py
@@ -0,0 +1,137 @@
+import socket, thread, sys, time, string
+from messageboard import postmessage, countmessages, listmessages, readmessage, delmessage
+from namekeeper import registernick, freenick, changenick, nickregistered, nicklist
+
+global outmessagetext, outmessageid
+
+port = 59387
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('', port))
+sock.listen(5) #listen for up to 5 new incoming requests at the same moment
+
+outmessagetext = ''
+outmessageid = -1
+def sendmessage(text, nick):
+    global outmessagetext, outmessageid
+    if not nickregistered(nick) and nick != "tehsrvr":
+        thread.exit_thread()
+    if not (text.startswith('!') and nick == 'tehsrvr'): #no timestamp on server commands
+        stamp = time.localtime()
+        asciistamp = "%d:%d" % (stamp.tm_hour, stamp.tm_min)
+        if stamp.tm_min < 10: asciistamp = string.replace(asciistamp, ':', ':0')
+        text = asciistamp + " " + text
+    print "BCAST: " + text
+    outmessagetext = text
+    outmessageid = outmessageid + 1
+
+def sendloop(c, nick):
+    global outmessagetext, outmessageid
+    lastmessageid = outmessageid
+    while 1:
+        if outmessageid != lastmessageid:
+            try:
+                c.send(outmessagetext)
+            except: #the client is no longer available
+                return
+            lastmessageid = outmessageid
+        else:
+            time.sleep(0.01)
+
+def getnick(c):
+    nick = c.recv(1024)
+    if len(nick) > 10:
+        nick = nick[:9]
+        c.send("!NOTE Nickname truncated to %s" % nick)
+    return nick
+
+def welcome(c):
+    time.sleep(0.1)
+    c.send('!NOTE Current users are: ' + string.join(nicklist(), ', '))
+    messages = countmessages()
+    if messages:
+        c.send('!NOTE There are %d saved messages!!!' % messages)
+
+def clientthread(c, ip):
+    try:
+        nick = getnick(c)
+    except:
+        print '%s getnick failed: %s' % (ip, str(sys.exc_info()[1]))
+        return
+    if registernick(nick) > 0: #if the nick is used
+        c.send("!NOTE Nickname not valid.")
+        c.send("!TERMINATE")
+        c.close()
+        return
+    print "%s logged in as %s" % (ip, nick)
+    sendmessage("%s joined!" % nick, 'tehsrvr')
+    c.send("!CHATMODE")
+    thread.start_new_thread(sendloop, (c, nick))
+    welcome(c)
+    while 1: #recv loop. spends most of time on c.recv()
+        try:
+            message = c.recv(1000)
+            if len(message) == 0: #client should not send empty messages
+                raise IOError, "Client exit."
+        except:
+            sendmessage("%s quit: %s" % (nick, str(sys.exc_info()[1])), "tehsrvr")
+            freenick(nick)
+            break
+        message = message.strip("\xaa")
+        if message.startswith("/me "):
+            message = string.replace(message, "/me ", "")
+            sendmessage(" * %s %s" % (nick,message), nick)
+        elif message == '/list':
+            c.send('!NOTE Current users: ' + string.join(nicklist(), ', '))
+        elif message.startswith('/nick '):
+            oldnick = nick
+            nick = string.split(message)[1]
+            changenick(oldnick, nick)
+            sendmessage(' The user %s is now known as %s' % (oldnick, nick), 'tehsrvr')
+        elif message == '/exit':
+            c.send('!TERMINATE')
+        #messageboard commands
+        elif message == '/msglist':
+            c.send('!NOTE Messages: '+listmessages())
+        elif message.startswith('/msgpost '):
+            try:
+                wordlist = string.split(message)
+                title = wordlist[1]
+                text = string.join(wordlist[2:])
+            except:
+                c.send('!NOTE Error parsing request: '+str(sys.exc_info()[1])+'\nYou might want to report this to the server operator.')
+                continue
+            c.send("!NOTE "+postmessage(title, text))
+        elif message.startswith('/msgread '):
+            try:
+                title = string.split(message)[1]
+            except IndexError:
+                c.send('!NOTE Missing argument!')
+                continue
+            c.send("!NOTE "+readmessage(title))
+        elif message.startswith('/msgdel '):
+            try:
+                title = string.split(message)[1]
+            except IndexError:
+                c.send('!NOTE Missing argument!')
+                continue
+            c.send("!NOTE "+delmessage(title))
+        else:
+            sendmessage("<%s> %s" % (nick, message), nick)
+
+def main():
+    print "Accepting clients from port %d" % port
+    while 1:
+        c, info = sock.accept()
+        print "Connection request from %s." % info[0]
+        thread.start_new_thread(clientthread, (c, info[0]))
+
+try:
+    main()
+except:
+    sock.close()
+    sendmessage("Server exit, disconnecting all clients...", 'tehsrvr')
+    time.sleep(1)
+    sendmessage("!TERMINATE", 'tehsrvr')
+    time.sleep(2)
+    print sys.exc_info()
+
---
+++ branches/crypt/tkclient.py
@@ -0,0 +1,90 @@
+from Tkinter import *
+import client, thread, time, ScrolledText
+
+class CloseNote:
+    def __init__(self, parent):
+        top = self.top = Toplevel(parent)
+        Label(top, text='You have been disconnected from the server.').pack()
+        b = Button(top, text="OK", command=self.ok)
+        b.pack(pady=5)
+
+    def ok(self):
+        self.top.destroy()
+
+class App:
+    def __init__(self, master):
+
+        root.resizable(width=FALSE, height=FALSE)
+
+        self.primary =      "#1C5FBA" #logo yellow
+        self.secondary =    "#F1E300" #logo blue
+
+        frame = Frame(master, bg= self.primary)
+        frame.pack()
+
+        self.box = ScrolledText.ScrolledText(frame, wrap=WORD, width = 80, relief = GROOVE, bg= self.primary, fg = self.secondary)
+        self.box.insert(END, 'Welcome to PyChat 2!')
+        self.box.config(state=DISABLED)
+        self.box.pack(side=TOP)
+
+        entryframe = Frame(master = frame)
+        entryframe.pack()
+
+        self.texvar = StringVar()
+        self.texvar.set("")
+        self.label = Label(entryframe, textvariable=self.texvar, bg = self.primary, fg = self.secondary)
+        self.label.pack(side=LEFT)
+
+        self.entry = Entry(entryframe, width = 50, bg= self.primary, fg = self.secondary)
+        self.entry.bind("<Return>", self.gettext)
+        self.entry.pack(side=LEFT)
+
+        self.quitter = Button(frame, relief = RAISED, text = "Quit", fg = self.secondary, bg = self.primary, command = self.quitbutton)
+        self.quitter.pack(side=RIGHT)
+
+        entryframe.pack(side=LEFT)
+
+        self.buffer = ''
+        self.bufferfull = False
+
+        thread.start_new_thread(client.callbacks, (self.printtex, self.blockinput, self.close))
+
+    def printtex(self, text):
+        self.box.config(state=NORMAL)
+        self.box.insert(END,"\n"+text)
+        self.box.config(state=DISABLED)
+        self.box.yview_scroll(1,"units")
+
+    def quitbutton(self):
+        root.quit()
+
+    def gettext(self, e):
+        text = self.entry.get()
+        self.entry.delete(0, END)
+        self.buffer = text
+        self.bufferfull = True
+
+    def blockinput(self, prompt=''):
+        if prompt != '':
+            self.texvar.set(prompt)
+        else:
+            self.texvar.set("Say:")
+        while 1:
+            if self.bufferfull:
+                self.bufferfull = False
+                text = self.buffer
+                self.buffer = ''
+                return text
+            else:
+                time.sleep(0.01)
+
+    def close(self):
+        dialog = CloseNote(root)
+        root.wait_window(dialog.top)
+        root.quit()
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
